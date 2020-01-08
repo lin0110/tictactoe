d2e339bc8476b9c4813fa3bd6539a5a9fda60444
lin0110 <jiajunlin2016@gmail.com>
2020-01-08 23:30:17 +0000

    Finish

--- js/script.js
+++ js/script.js
@@ -80,47 +80,38 @@ function checkDiagonalWins() {
 
 
 $("#tile1").click(function () {
-    performLogic("#button1", "#tile1");
-    
+    performLogic("#button1", "#tile1"); 
 });
 
 $("#tile2").click(function () {
-    performLogic("#button2", "#tile2");
-   
+    performLogic("#button2", "#tile2"); 
 });
 
 $("#tile3").click(function () {
     performLogic("#button3", "#tile3");
-
 });
 
 $("#tile4").click(function () {
-    performLogic("#button4", "#tile4");
-    
+    performLogic("#button4", "#tile4");    
 });
 
 $("#tile5").click(function () {
     performLogic("#button5", "#tile5");
-
 });
 
 $("#tile6").click(function () {
-    performLogic("#button6", "#tile6");
-    
+    performLogic("#button6", "#tile6"); 
 });
 
 $("#tile7").click(function () {
     performLogic("#button7", "#tile7");
-
 });
 
 $("#tile8").click(function () {
-    performLogic("#button8", "#tile8");
-   
+    performLogic("#button8", "#tile8");  
 });
 
 $("#tile9").click(function () {
     performLogic("#button9", "#tile9");
- 
 });
 
